b49dce27c3450bcc790c46415206d2234bebc508
Natalia Zolotovitskaya <natalia.zolotovitskaya@infotecs.ru>
2017-11-30 09:48:11 +0300

    working with json

--- microservices/hello-microservice-http/src/main/java/ru/u26c2/vertx/book/http/HelloMicroservice.java
+++ microservices/hello-microservice-http/src/main/java/ru/u26c2/vertx/book/http/HelloMicroservice.java
@@ -1,18 +1,32 @@
 package ru.u26c2.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class HelloMicroservice extends AbstractVerticle {
 
     @Override
     public void start() {
         Router router = Router.router(vertx);
-        router.get("/").handler(rc -> rc.response().end("hello"));
-        router.get("/:name").handler(rc -> rc.response().end("hello " + rc.pathParam("name")));
+        router.get("/").handler(this::hello);
+        router.get("/:name").handler(this::hello);
 
         vertx.createHttpServer()
                 .requestHandler(router::accept)
                 .listen(9090);
+    }
+
+    private void hello(RoutingContext rc) {
+        String message = "hello";
+        if (rc.pathParam("name") != null) {
+            message += " " + rc.pathParam("name");
+        }
+        JsonObject json = new JsonObject().put("message", message);
+        rc.response()
+                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .end(json.encode());
     }
 }
